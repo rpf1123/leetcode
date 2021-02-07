a78b9a7d1e940be82c1f05de8efdbb26db4a9386
aytida <aditya281091@gmail.com>
2021-02-06 23:50:04 -0800

    Minimum Deviation Array

---
+++ src/main/java/minimizedeviationinarray/Solution.java
@@ -0,0 +1,10 @@
+package minimizedeviationinarray;
+
+/**
+ * https://leetcode.com/problems/minimize-deviation-in-array/
+ */
+class Solution {
+    public int minimumDeviation(int[] nums) {
+        return 0;
+    }
+}
